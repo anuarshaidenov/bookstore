f500eb2ef73f5f7a5996a435547e257621162f67
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-01-24 23:28:07 +0600

    Dispatch add book

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,11 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 import booksReducer from './books/books';
 
 const combinedReducers = combineReducers({
   books: booksReducer,
 });
 
-const store = createStore(combinedReducers);
+const store = createStore(combinedReducers, applyMiddleware(logger));
 
 export default store;
