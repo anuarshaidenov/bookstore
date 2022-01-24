f2f26747f4cc752bdd5bc69723ec7b0152bedba5
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-01-24 23:34:08 +0600

    Dispatch remove book

--- src/components/bookSingle/BookSingle.js
+++ src/components/bookSingle/BookSingle.js
@@ -1,21 +1,33 @@
 import './book-single.scss';
+
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../../redux/books/books';
 
 const BookSingle = (book) => {
   const {
-    book: { title },
+    book: { id, title },
   } = book;
+
+  const dispatch = useDispatch();
+
+  const handleRemoveBook = () => {
+    dispatch(removeBook(id));
+  };
 
   return (
     <li>
       {title}
-      <button type="button">remove</button>
+      <button onClick={handleRemoveBook} type="button">
+        remove
+      </button>
     </li>
   );
 };
 
 BookSingle.propTypes = {
-  book: PropTypes.shape({ title: PropTypes.string }).isRequired,
+  book: PropTypes.shape({ id: PropTypes.string, title: PropTypes.string })
+    .isRequired,
 };
 
 export default BookSingle;
